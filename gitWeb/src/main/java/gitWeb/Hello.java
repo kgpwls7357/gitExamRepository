0727ea89fc2c44205bb4d638e7dd2cd4e858831f
paramy <rimiajung@gmail.com>
2024-03-22 14:50:10 +0900

    14.49

--- gitWeb/src/main/java/gitWeb/Hello.java
+++ gitWeb/src/main/java/gitWeb/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
   public static void main(String [] args) {
 	  System.out.println("혜림님 안녕하세요~!");
 	  System.out.println("점심 맛있게 드셨어요?");
+	  System.out.println("존맛탱구리~~");
   }
 }
